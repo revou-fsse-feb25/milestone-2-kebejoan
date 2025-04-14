1451f737889bc6966400a0ced6a615eb6398f5dd
kebejoan <jmailed.joe@gmail.com>
2025-04-15 00:59:35 +0700

    add: install tailwind and develop homepage

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./**/*.html"], // make sure Tailwind scans your HTML files
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
