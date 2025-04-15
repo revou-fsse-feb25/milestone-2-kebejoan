a1ba6e504c57b8bb05cb0a6fe0f404af1a34c230
kebejoan <jmailed.joe@gmail.com>
2025-04-16 00:11:03 +0700

    mod: html and ts for clicking game

--- scripts/clicking-game.js
+++ scripts/clicking-game.js
@@ -1,9 +1,29 @@
-var minimumResolutionMessage = "Minimum Resolution to play this game is 1920x1080";
-console.log(minimumResolutionMessage);
-var div = document.getElementById("click-me");
-if (div) {
-    div.addEventListener("click", function (event) {
-        console.log("Div was clicked", event);
-        alert("Div was clicked");
+var minimumResolutionMessage = "Minimum Resolution to play this game is 1920x1080! \nAdjust with ctrl+wheel if needed";
+alert(minimumResolutionMessage);
+var clickMe = document.getElementById("click-me");
+var restartButton = document.getElementById("restart");
+var score = document.getElementById("score");
+var time = document.getElementById("time");
+var scoreValue = 0;
+function updateScoree() {
+    scoreValue++;
+    if (score) {
+        score.textContent = "SCORE: " + scoreValue.toString();
+    }
+}
+function resetScore() {
+    scoreValue = 0;
+    if (score) {
+        score.textContent = "SCORE: 0";
+    }
+}
+if (clickMe) {
+    clickMe.addEventListener("click", function () {
+        updateScoree();
+    });
+}
+if (restartButton) {
+    restartButton.addEventListener("click", function () {
+        resetScore();
     });
 }
--- scripts/clicking-game.ts
+++ scripts/clicking-game.ts
@@ -1,11 +1,35 @@
-var minimumResolutionMessage: string = "Minimum Resolution to play this game is 1920x1080";
-console.log(minimumResolutionMessage);
+var minimumResolutionMessage: string = `Minimum Resolution to play this game is 1920x1080! 
+Adjust with ctrl+wheel if needed`;
+alert(minimumResolutionMessage);
 
-const div = document.getElementById("click-me");
+var clickMe = document.getElementById("click-me");
+var restartButton = document.getElementById("restart");
+var score = document.getElementById("score");
+var time = document.getElementById("time");
+var scoreValue : number = 0;
 
-if (div){
-    div.addEventListener("click", (event: MouseEvent) => {
-        console.log("Div was clicked", event);
-        alert("Div was clicked");
+function updateScoree() : void{
+    scoreValue++;
+    if (score){
+        score.textContent = "SCORE: " + scoreValue.toString();
+    } 
+}
+
+function resetScore() : void{
+    scoreValue = 0;
+    if (score){
+        score.textContent = "SCORE: 0";
+    }
+}
+
+if (clickMe){
+    clickMe.addEventListener("click", () => {
+        updateScoree();
+    });
+}
+
+if (restartButton){
+    restartButton.addEventListener("click", () => {
+        resetScore();
     });
 }
